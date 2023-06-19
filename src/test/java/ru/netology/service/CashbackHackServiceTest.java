cc6238a99e2372052736f5aeea9e66b15c2e705f
Ksenya Sunduova <avissimplex@gmail.com>
2023-06-19 23:46:01 +0400

    Testng commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,48 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testShouldReturn100ifAmount900() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testShouldReturn100ifAmount1900() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(1900);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testShouldReturn0ifAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testShouldReturn999ifAmount1001() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testShouldReturn1000ifAmount0() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(actual, expected);
+    }
+
+}
